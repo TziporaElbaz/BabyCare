2ab72a348f3c5e96cfc0e6c9ce7250241ce8e542
NoaWajesberg <Hand3D14@mbjcomp.org.il>
2025-05-06 17:01:20 +0300

    עדכון שינויים מקומיים

---
+++ Project/DAL/API/IAppointmentManagementDAL.cs
@@ -0,0 +1,16 @@
+﻿using Project.DAL.models;
+
+namespace Project.DAL.services
+{
+    public interface IAppointmentManagementDAL
+    {
+        Task AddAppointmentAsync(Appointment appointment);
+        Task DeleteAppointmentAsync(int id);
+        Task<List<Appointment>> GetAllAppointmentsAsync();
+        Task<Appointment?> GetAppointmentByIdAsync(int id);
+        Task<Appointment?> GetAppointmentByWorkerAndDatetime(int workerId, DateOnly date, TimeOnly time);
+        Task<List<Appointment>> GetAppointmentsByDateAsync(DateOnly date);
+        Task<bool> IsTimeSlotAvailableAsync(DateOnly date, TimeOnly startTime, TimeOnly endTime);
+        Task UpdateAppointmentAsync(Appointment updatedAppointment);
+    }
+}
