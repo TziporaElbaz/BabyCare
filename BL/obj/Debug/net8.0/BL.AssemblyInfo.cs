e3e3305d38eebbb6a2d1326d4136550f2c189cb6
NoaWajesberg <Hand3D14@mbjcomp.org.il>
2025-05-27 20:26:33 +0300

    change

--- BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a281b664f78c212800935bbbbec40032451f3cea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c0e2aad3051758caed5342b0dffd5ad5154b32")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
